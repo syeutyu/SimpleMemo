7d3b0aa69216c63354cdb8740abeffb5107d2678
syeutyu <syeuty@naver.com>
2018-05-10 11:15:02 +0900

    [Update] DataBindingUtil을 이용한 Findbyid의 중복 제거 및 적용

--- app/src/main/java/com/example/lee/myapplication/Main.java
+++ app/src/main/java/com/example/lee/myapplication/Main.java
@@ -1,67 +1,63 @@
 package com.example.lee.myapplication;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import com.example.lee.myapplication.Domain.MemoItem;
+import com.example.lee.myapplication.databinding.ContentMainBinding;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.*;
 
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class Main extends AppCompatActivity {
 
     public static final String TAG = "TAG";
     private Boolean isFabOpen = false;
     private Animation fab_open, fab_close, rotate_forward, rotate_backward;
-    private FloatingActionButton mainFab, memoFab, settingFab;
-    private Intent intent;
-    private RecyclerView recyclerView;
     private Adapter adapter;
-    private GridLayoutManager manager;
-    private static List<RecyclerItem> list;
-    private Realm realm;
+    private List<MemoItem> list;
     private DataHelper helper;
+    public  Realm realm;
+    private ContentMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.content_main);
         Realm.init(this);
         activityInit();
-        for(Entry<String,String> data : helper.get(realm).entrySet()){
-            System.out.println(data.getKey()+"  :  "+  data.getValue());
-        }
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(adapter);
-        mainFab.setOnClickListener(new View.OnClickListener() {
+        mainBinding.recyclerView.setHasFixedSize(true);
+        mainBinding.recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+        mainBinding.recyclerView.setAdapter(adapter);
+        mainBinding.plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setAnimation();
             }
         });
 
-        memoFab.setOnClickListener(new View.OnClickListener() {
+        mainBinding.memo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(getApplicationContext(), Memo.class);
+                Intent intent = new Intent(getApplicationContext(), Memo.class);
                 setAnimation();
                 startActivity(intent);
             }
         });
 
-        settingFab.setOnClickListener(new View.OnClickListener() {
+        mainBinding.setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(getApplicationContext(), Setting.class);
+                Intent intent = new Intent(getApplicationContext(), Setting.class);
                 setAnimation();
                 startActivity(intent);
             }
@@ -71,46 +67,37 @@ public class Main extends AppCompatActivity {
 
     private void activityInit() {
         realm = Realm.getDefaultInstance();
-        list = new ArrayList<>();
         helper = new DataHelper();
         helper.create(realm);
-        insertValue();
+        list = new ArrayList<>();
+        RealmResults<MemoItem> check = realm.where(MemoItem.class).findAll();
+        list.addAll(check);
+        adapter = new Adapter(list);
+
         rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
         rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
-        recyclerView = findViewById(R.id.recyclerView);
-        mainFab = findViewById(R.id.plus);
-        memoFab = findViewById(R.id.memo);
-        settingFab = findViewById(R.id.setting);
-        adapter = new Adapter(list);
-        manager = new GridLayoutManager(getApplicationContext(),2);
     }
 
 
     private void setAnimation() {
         if (isFabOpen) {
-            mainFab.startAnimation(rotate_backward);
-            memoFab.startAnimation(fab_close);
-            settingFab.startAnimation(fab_close);
+            mainBinding.plus.startAnimation(rotate_backward);
+            mainBinding.memo.startAnimation(fab_close);
+            mainBinding.setting.startAnimation(fab_close);
             isFabOpen = false;
-            memoFab.setClickable(isFabOpen);
-            settingFab.setClickable(isFabOpen);
+            mainBinding.memo.setClickable(isFabOpen);
+            mainBinding.setting.setClickable(isFabOpen);
         } else {
-            mainFab.startAnimation(rotate_forward);
-            memoFab.startAnimation(fab_open);
-            settingFab.startAnimation(fab_open);
+            mainBinding.plus.startAnimation(rotate_forward);
+            mainBinding.memo.startAnimation(fab_open);
+            mainBinding.setting.startAnimation(fab_open);
             isFabOpen = true;
-            memoFab.setClickable(isFabOpen);
-            settingFab.setClickable(isFabOpen);
+            mainBinding.memo.setClickable(isFabOpen);
+            mainBinding.setting.setClickable(isFabOpen);
         }
 
-    }
-
-    private void insertValue() {
-        for (int i = 0; i < 6; i++) {
-            list.add(new RecyclerItem("title : " + String.valueOf(i), " body : " + String.valueOf(i)));
-        }
     }
 
     @Override
--- app/src/main/java/com/example/lee/myapplication/Memo.java
+++ app/src/main/java/com/example/lee/myapplication/Memo.java
@@ -1,13 +1,36 @@
 package com.example.lee.myapplication;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.lee.myapplication.databinding.MemoBinding;
+
+import io.realm.Realm;
+
 
 public class Memo extends AppCompatActivity {
+    private MemoBinding memoBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memo);
+        memoBinding = DataBindingUtil.setContentView(this,R.layout.memo);
+
+//        memoBinding.saveBtn.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                    Main.realm.executeTransactionAsync(new Realm.Transaction() {
+////                        @Override
+////                        public void execute(Realm realm) {
+////                            Data data = new Data();
+////                            data.setTitle(memoBinding.inputTitle.getText().toString());
+////                            data.setBody(memoBinding.inputBody.getText().toString());
+////                        }
+////                    });
+////            }
+////        })
+
     }
 }
