9ec877b2ad2b0b318d2d1cfbd8dc4a4267d3b509
syeutyu <syeuty@naver.com>
2018-05-24 23:44:59 +0900

    [Add] Adapter에서 view를 통한 접근방식 변경

--- app/src/main/java/com/example/lee/myapplication/Adapter.java
+++ app/src/main/java/com/example/lee/myapplication/Adapter.java
@@ -1,55 +1,56 @@
 package com.example.lee.myapplication;
 
+import android.annotation.SuppressLint;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.lee.myapplication.Domain.MemoItem;
+import com.example.lee.myapplication.databinding.ItemCardBinding;
 
 import java.util.List;
 
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-   private List<MemoItem> list;
+    private List<MemoItem> list;
+    @SuppressLint("StaticFieldLeak")
+    static ItemCardBinding cardBinding;
 
-    public Adapter(List<MemoItem> list){
+    Adapter(List<MemoItem> list) {
         this.list = list;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-         TextView title;
-         TextView body;
-         CardView cardView;
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(View v){
+        ViewHolder(View v) {
             super(v);
-            title = itemView.findViewById(R.id.cardTitle);
-            body = itemView.findViewById(R.id.cardBody);
-            cardView = itemView.findViewById(R.id.card);
+            cardBinding = DataBindingUtil.bind(v);
         }
-
     }
 
+    public void addData(MemoItem item){
+        list.add(item);
+    }
+
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
-        return new ViewHolder(v);
+        return new ViewHolder( LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.title.setText(list.get(position).getTitle());
-        holder.body.setText(list.get(position).getBody());
-        holder.cardView.setOnClickListener(new View.OnClickListener() {
+        cardBinding.cardTitle.setText(list.get(position).getTitle());
+        cardBinding.cardBody.setText(list.get(position).getBody());
+        cardBinding.card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v,"카드 내용",Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(v, "카드 내용", Snackbar.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/lee/myapplication/Memo.java
+++ app/src/main/java/com/example/lee/myapplication/Memo.java
@@ -18,19 +18,19 @@ public class Memo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         memoBinding = DataBindingUtil.setContentView(this,R.layout.memo);
 
-//        memoBinding.saveBtn.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View v) {
-////                    Main.realm.executeTransactionAsync(new Realm.Transaction() {
-////                        @Override
-////                        public void execute(Realm realm) {
-////                            Data data = new Data();
-////                            data.setTitle(memoBinding.inputTitle.getText().toString());
-////                            data.setBody(memoBinding.inputBody.getText().toString());
-////                        }
-////                    });
-////            }
-////        })
+        memoBinding.saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Main.realm.executeTransactionAsync(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            Data data = new Data();
+                            data.setTitle(memoBinding.inputTitle.getText().toString());
+                            data.setBody(memoBinding.inputBody.getText().toString());
+                        }
+                    });
+            }
+        });
 
     }
 }
